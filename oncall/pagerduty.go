38e912ec869d773e40dc49753493ee3bef6b64eb
Mark Cabanero <mark.cabanero@clever.com>
2020-10-29 18:42:22 -0700

    feat: Add scaffolding for oncall package
    This gives us the high-level steps required for PagerDuty calls, and having it
    correspond with what's already part of the bot.

---
+++ oncall/pagerduty.go
@@ -0,0 +1,7 @@
+package oncall
+
+// Things to do:
+// 1. Client to interact with PagerDuty (https://godoc.org/github.com/PagerDuty/go-pagerduty#NewOAuthClient)
+// 2. Call ListOnCallUsers (https://godoc.org/github.com/PagerDuty/go-pagerduty#Client.ListOnCallUsers)
+// 3. Somehow merge the two lists (ListOnCallUsers vs who-is-who's representation)?
+// 4. Return this to be stored within the bot's structure.
