fca097aa3c24c480ba9554acd088caeda7b58c1f
Mark Cabanero <mark.cabanero@clever.com>
2021-02-23 18:28:06 -0800

    dep: dep -> go mod

---
+++ tools/tools.go
@@ -0,0 +1,8 @@
+package tools
+
+import (
+	// Blank import ensures that these repos are included in go.mod, so we can
+	// build CLI tools from the ./vendor/ directory during `make install_deps`
+	_ "github.com/golang/mock/gomock"
+	_ "github.com/golang/mock/mockgen"
+)
