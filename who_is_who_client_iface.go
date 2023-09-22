e4d6c1152357300af64ea24781337812ec938a2c
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2023-09-22 17:07:38 -0400

    upgrade to go 1.21

--- who_is_who_client_iface.go
+++ who_is_who_client_iface.go
@@ -3,7 +3,7 @@ package main
 import whoswho "github.com/Clever/who-is-who/go-client"
 
 // generate mocks of dependencies for use during testing
-//go:generate sh -c "$PWD/bin/mockgen -package main -source $PWD/who_is_who_client_iface.go whoIsWhoClientIface > who_is_who_client_mock_test.go"
+//go:generate sh -c "bin/mockgen -package main -source who_is_who_client_iface.go whoIsWhoClientIface > who_is_who_client_mock_test.go"
 
 type whoIsWhoClientIface interface {
 	UserBySlackID(string) (whoswho.User, error)
